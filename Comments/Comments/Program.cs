ccad1b90d7b0acf49fd2f80ad642d716e851c057
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2021-06-14 10:06:52 +0200

    Comments

---
+++ Comments/Comments/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // for single line comments  
+
+            /* for multi line comments */
+
+            /// XML tags displayed in a code comment
+            
+            /*
+             Write comments as if they were for you: (in fact, they are)
+
+            When it comes to commenting code, think not only about the developers who will maintain your code in the future, but also think about yourself. 
+            In the words of the great Phil Haack:
+
+            "As soon as a line of code is laid on the screen, you're in maintenance mode on that piece of code."
+
+            As a result, we ourselves will be the first beneficiaries (or victims) of our good (or bad) comments.
+
+            Write a comment in a polite manner:
+
+            Avoid rude and harsh comments:
+            You never know who may read these comments in the future: your boss, a customer, or the pathetically inept developer you just insulted. 
+            So maintain your respect and dignity.
+
+            The golden rule:
+
+            Let the code speak for itself. 
+            You code should be written in such a way that everyone should easily understand it.
+             */
+
+        }
+    }
+}
