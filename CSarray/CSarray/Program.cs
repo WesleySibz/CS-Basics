4b0e12bea7b0956068682411a74b089e5de3416d
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2021-06-14 12:34:59 +0200

    C# Array

---
+++ CSarray/CSarray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CSarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            An array is a collection of similar types of data.
+            For example, 
+            if we want to store the names of 100 people then we can create an array of the string type that can store 100 names.
+            String[] array = new String[100];
+
+            How to declare an array:
+            dataType[] arrayName;
+
+            To define the number of elements that an array can hold, we have to allocate memory for the array in C#.
+            // declare an array
+            double[] data;
+
+            // allocate memory
+            data = new Double[10];
+
+            In c#, we can initialize arrays during declaration:
+             //declare and initialize and array
+            int[] age = {12, 4, 5, 2, 5};
+
+            Here, we have created an array named age and initialized it with the values inside the curly brackets.
+            Note that we have not provided the size of the array. 
+            In this case, the Java compiler automatically specifies the size by counting the number of elements in the array (i.e. 5).
+            In the Java array, each memory location is associated with a number. 
+            The number is known as an array index.
+            We can also initialize arrays in c#, using the index number. 
+            // declare an array
+            int[] age = new int[5];
+
+            // initialize array
+            age[0] = 12;
+            age[1] = 4;
+            age[2] = 5;
+             */
+
+        }
+    }
+}
