07def89393e945fb29ba5e56911b618a2dba6ced
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2021-07-19 16:39:08 +0200

    Methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             A method is defined with the name of the method,
+            followed by parentheses (). C# provides some pre-defined methods,
+            which you already are familiar with, such as Main(),
+            but you can also create your own methods to perform certain actions:
+
+            class Program
+            {
+              static void MyMethod() 
+              {
+                // code to be executed
+              }
+            }
+
+            To call (execute) a method, 
+            write the method's name followed by two parentheses () and a semicolon;
+            In the following example, MyMethod() is used to print a text (the action),
+            when it is called:
+
+            static void MyMethod() 
+            {
+              Console.WriteLine("I just got executed!");
+            }
+
+            static void Main(string[] args)
+            {
+              MyMethod();
+            }
+
+             */
+        }
+    }
+}
